45c5bd135a5c1d27dd83c93d83ff5daf67278a78
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-03-18 10:50:18 -0300

    arquivo redirects criado

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { GlobalStyle } from './style/globalStyle';
 import { Header } from './components/header';
 import { ThemeProvider } from './context/themeContext';
 import { Nav } from './components/nav';
-import { ToggleThemeButton } from './components/toggleThemeButton.jsx';
-import { ProfilePicture } from './components/profilePicture/index.jsx';
 import { AppRoutes } from './pages/routes.jsx';
 
 function App() {
